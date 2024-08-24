e5f0f87189b2580c886b98bec9217ad33c40df6a
Jhoset NG <adm_dng@hotmail.com>
2024-08-24 12:21:46 -0500

    Implement Auth Microservice with JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,117 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { RpcException } from '@nestjs/microservices';
+import { LoginUserDto, RegisterUserDto } from './dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces';
+import { envs } from '../config';
 
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger('AuthService');
 
-  constructor() {
+  constructor(private readonly jwtService: JwtService) {
     super();
   }
 
   onModuleInit(): any {
     this.$connect();
     this.logger.log(`MongoDD success connection`);
+  }
+
+  async signJwt(payload: JwtPayload) {
+    return this.jwtService.sign(payload);
+  }
+
+  async registerUser(registerUserDto: RegisterUserDto) {
+    const { email, name, password } = registerUserDto;
+    try {
+      const user = await this.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      if (user) {
+        throw new RpcException({
+          status: HttpStatus.BAD_REQUEST,
+          message: 'Already exists a user with the email provided',
+        });
+      }
+
+      const createdUser = await this.user.create({
+        data: {
+          email,
+          password: bcrypt.hashSync(password, 10),
+          name,
+        },
+      });
+      const { password: __, ...rest } = createdUser;
+
+      return {
+        user: rest,
+        token: await this.signJwt(rest),
+      };
+    } catch (e) {
+      throw new RpcException({
+        status: e.error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
+        message: e.message,
+      });
+    }
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+    try {
+      const user = await this.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      if (!user) {
+        throw new RpcException({
+          status: HttpStatus.BAD_REQUEST,
+          message: 'Email or password is incorrect',
+        });
+      }
+      const isPasswordValid = bcrypt.compareSync(password, user.password);
+      if (!isPasswordValid) {
+        throw new RpcException({
+          status: HttpStatus.BAD_REQUEST,
+          message: 'Email or password is incorrect',
+        });
+      }
+
+      const { password: __, ...rest } = user;
+
+      return {
+        user: rest,
+        token: await this.signJwt(rest),
+      };
+    } catch (e) {
+      throw new RpcException({
+        status: e.error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
+        message: e.message,
+      });
+    }
+  }
+
+  async verifyToken(token: string) {
+    try {
+      const { sub, iat, exp, ...user } = this.jwtService.verify(token, {
+        secret: envs.jwt_secret,
+      });
+      return {
+        user,
+        token: await this.signJwt(user),
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: HttpStatus.UNAUTHORIZED,
+        message: 'Invalid Token',
+      });
+    }
   }
 }
--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,12 +5,14 @@ import * as process from 'node:process';
 interface EnvVars {
   PORT: number;
   NATS_SERVERS: string[];
+  JWT_SECRET: string;
 }
 
 const envSchema = joi
   .object({
     PORT: joi.number().required(),
     NATS_SERVERS: joi.array().items(joi.string().required()),
+    JWT_SECRET: joi.string().required(),
   })
   .unknown(true);
 
@@ -28,4 +30,5 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   natsServers: envVars.NATS_SERVERS,
+  jwt_secret: envVars.JWT_SECRET,
 };
